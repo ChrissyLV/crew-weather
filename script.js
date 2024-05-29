951b91a6a510278f11382b755a6e993f19fc9b37
willcodinggod white <walidwhite@live.co.uk>
2024-05-29 14:04:16 +0000

    debug2

--- script.js
+++
@@ -1,8 +0,0 @@
-<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-</div>
-</div>
-</div>
-</div>
-
-                                    <!-- Feedback Modal -->
-<div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
